a004091bf1feccb8319c23d6d01995e2facfd96d
jkpatilgit <106595659+jkpatilgit@users.noreply.github.com>
2024-04-28 11:12:41 +0000

    Improvements_28_APR_2024

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+JOBS = [
+  { 'id' : 1 ,
+    'title' : 'Data Analyst',
+    'location': 'Delhi, India',
+    'salary' : 'Rs. 10,00000'
+  },
+  { 'id' : 2 , 
+    'title' : 'Data Engineer',
+    'location': 'Bengaluru, India',
+    'salary' : 'Rs. 50,00000'
+  },
+{ 'id' : 3 ,
+  'title' : 'Data Scientist',
+  'location': 'Bengaluru, India',
+  'salary' : 'Rs. 70,00000'
+},
+{ 'id' : 4 ,
+  'title' : 'Backend Engineer',
+  'location': 'Bengaluru, India',
+  'salary' : 'Rs. 90,00000'
+}
+  
+]
 @app.route("/")
 def hello_world():
-   return render_template('home.html')
+   return render_template('home.html', 
+                          jobs=JOBS, company_name = 'JK Data')
 
 print(__name__)
 if __name__ =="__main__":
